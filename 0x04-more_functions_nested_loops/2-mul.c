0b9b92a51494c467103231c6bc14397a07488225
AbdulQudus01 <abdulquduskasali@gmail.com>
2022-09-15 07:51:25 -0700

    More Functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers.
+ *
+ * @a: First number.
+ *
+ * @b: Second number.
+ *
+ * Return: multiplication.
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a README file on More Functions and Nested Loops at the ALX Software Programming Course
